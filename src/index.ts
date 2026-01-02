609040b54763e27310ebc35429f921e65078918c
sharky <sharky@furshark.net>
2026-01-02 22:10:31 +0100

    added rate limits

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,28 @@ import express from "express";
 import { getMessageData, insertMessage } from "./guestbook_database.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import { rateLimit } from "express-rate-limit";
 
 dotenv.config({ quiet: true });
-
 const DEV_ENV = process.env.DEV_ENV;
 
 const app = express();
 const port = 3000;
 app.use(express.json());
 app.use(cors());
+
+if (!DEV_ENV) {
+  const limiter = rateLimit({
+    windowMs: 5 * 60 * 1000, // 5 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+    // store: ... , // Redis, Memcached, etc. See below.
+  });
+
+  app.use(limiter);
+}
 
 app.get("/", (req, res): void => {
   res.json("Hello, api!");
